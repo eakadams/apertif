7d2cb289558a8ca248b3003e92e6e40868897d48
Betsey Adams <adams@MacAdams.local>
2018-03-13 18:55:11 +0100

    Finished code for getting data into proper format

--- AAF/script_get_data_for_stefan.py
+++ AAF/script_get_data_for_stefan.py
@@ -19,8 +19,75 @@ cfg2 = '180302009.cfg'
 #Prepare data using Apercal
 #This copies to directory defined in cfgfile
 
+prepare = apercal.prepare(cfg1)
+prepare.go()
+
+prepare = apercal.prepare(cfg2)
+prepare.go()
 
 
+#Then convert the data to miriad file
+convert = apercal.convert(cfg1)
+convert.go()
 
-#Need Galactic HI channels
-#These are: 
+convert = apercal.convert(cfg2)
+convert.go()
+
+
+#Now in miriad need to use uvimage to create image cube
+#Want to limit number of channels
+#Want to include near 9330, 20 subbands
+#Subbands start at multiples of 64
+#take subbands 130-149
+#channels 8320, 9599
+
+uvimage = lib.miriad('uvimage')
+#move to directory where data is
+ccal = apercal.ccal(cfg1)
+ccal.director('ch', ccal.crosscaldir)
+uvimage.vis = ccal.target
+uvimage.line='channel,1280,8320,1'
+uvimage.view = 'amplitude'
+targetname = ccal.target[:-4]
+uvimage.out= targetname+'_amp.im'
+uvimage.go()
+
+#write to fits file
+fits = lib.miriad('fits')
+fits.in_ = uvimage.out
+fits.op = "xyout"
+fits.out = targetname+'_amp.fits'
+fits.go()
+
+#now do phase
+uvimage.view='phase'
+uvimage.out=targetname+'_phase.im'
+uvimage.go()
+
+fits.in_ = uvimage.out
+fits.out = targetname+'_phase.fits'
+fits.go()
+
+#and now do the second source
+ccal = apercal.ccal(cfg2)
+ccal.director('ch', ccal.crosscaldir)
+uvimage.vis = ccal.target
+#uvimage.line='channel,1280,8320,1'
+uvimage.view = 'amplitude'
+targetname = ccal.target[:-4]
+uvimage.out= targetname+'_amp.im'
+uvimage.go()
+
+fits.out = targetname+'_amp.fits'
+fits.go()
+
+#and phase of second source
+uvimage.view='phase'
+uvimage.out=targetname+'_phase.im'
+uvimage.go()
+
+fits.in_ = uvimage.out
+fits.out = targetname+'_phase.fits'
+fits.go()
+
+#And that should be everything
