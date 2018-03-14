c2759e9c0bd55cf9e42b4d6844405ce423c50cc5
eakadams <37330078+eakadams@users.noreply.github.com>
2018-03-14 09:13:06 +0100

    Tuesday evening changes

--- AAF/script_get_data_for_stefan.py
+++ AAF/script_get_data_for_stefan.py
@@ -12,13 +12,13 @@ import apercal
 #180216005 (Dwingeloo 1)
 #180302009 (DR21)
 #Will create almost empty configuration files for both
-cfg1 = '180216005.cfg'
-cfg2 = '180302009.cfg'
+cfg1 = '/home/adams/apertif/AAF/180216005.cfg'
+cfg2 = '/home/adams/apertif/AAF/180302009.cfg'
 
 
 #Prepare data using Apercal
 #This copies to directory defined in cfgfile
-
+"""
 prepare = apercal.prepare(cfg1)
 prepare.go()
 
@@ -32,7 +32,7 @@ convert.go()
 
 convert = apercal.convert(cfg2)
 convert.go()
-
+"""
 
 #Now in miriad need to use uvimage to create image cube
 #Want to limit number of channels
@@ -41,16 +41,21 @@ convert.go()
 #take subbands 130-149
 #channels 8320, 9599
 
+print 'starting to get data'
+
 uvimage = lib.miriad('uvimage')
 #move to directory where data is
 ccal = apercal.ccal(cfg1)
 ccal.director('ch', ccal.crosscaldir)
 uvimage.vis = ccal.target
+print uvimage.vis
 uvimage.line='channel,1280,8320,1'
 uvimage.view = 'amplitude'
 targetname = ccal.target[:-4]
 uvimage.out= targetname+'_amp.im'
 uvimage.go()
+
+print 'got uv amp image'
 
 #write to fits file
 fits = lib.miriad('fits')
@@ -58,6 +63,8 @@ fits.in_ = uvimage.out
 fits.op = "xyout"
 fits.out = targetname+'_amp.fits'
 fits.go()
+
+print 'got fits image'
 
 #now do phase
 uvimage.view='phase'
