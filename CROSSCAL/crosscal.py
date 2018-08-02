895c98f9af1172909e1cee1ce03d35f9135d5ddc
eakadams <37330078+eakadams@users.noreply.github.com>
2018-08-02 12:52:47 +0200

    Creation of crosscal task

---
+++ CROSSCAL/crosscal.py
@@ -0,0 +1,3 @@
+#python "module" for tests and inventories related to
+#confirming the ability and calculating appropriate scaling
+#for transfering solutions from central beam to other beams
---
+++ CROSSCAL/test_crosscal.py
@@ -0,0 +1,1 @@
+#collection of tests for crosscal.py
