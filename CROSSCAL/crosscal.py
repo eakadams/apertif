29c4ce0b79e2a505e4fe0310d7e850ef81a65538
eakadams <37330078+eakadams@users.noreply.github.com>
2018-08-03 17:07:04 +0200

    ScanSpecification Class and tests

--- CROSSCAL/crosscal.py
+++ CROSSCAL/crosscal.py
@@ -1,3 +1,212 @@
 #python "module" for tests and inventories related to
 #confirming the ability and calculating appropriate scaling
 #for transfering solutions from central beam to other beams
+
+#define a class that is used for specifying the scans to be investigated
+
+class ScanSpecification(object):
+    def __init__(self):
+        #set all attributes to empty strings to initialize
+        #that way I can check for what is set later
+        self.startdate=''
+        self.enddate=''
+        self.beam=''
+        self.startscan=''
+        self.endscan=''
+        self.nscan=''
+    def setstartdate(self,sd):
+        #want to do some checking that startdate is in the right format
+        #turn into a string
+        sdstr=str(sd)
+        if len(sdstr) != 8:
+            print "Startdate must be  of format 'YYYYMMDD'"
+        elif sdstr[0:3] != '201':
+            print "Startdate must be a string of format 'YYYYMMDD', starting from 2010"
+        else:
+            self.startdate=sdstr
+    def setenddate(self,ed):
+        edstr=str(ed)
+        if len(edstr) != 8:
+            print "Enddate must be of format 'YYYYMMDD'"
+        elif edstr[0:3] != '201':
+            print "Enddate must be of format 'YYYYMMDD', starting from 2010"
+        else:
+            self.enddate = edstr
+    def setbeam(self,bm):
+        bmstr=str(bm)
+        if len(bmstr) == 1:
+            newbmstr = '{0}{1}'.format(0,bmstr)
+            self.beam = newbmstr
+        elif float(bmstr) != int(float(bmstr)):
+            print "Beam must be an integer"
+        elif len(bmstr) != 2:
+            print "Beam must be of format 'NN'"
+        elif float(bmstr) >39 or float(bmstr) <0:
+            print "Beam must be an integer between 0 and 39"
+        else:
+            self.beam = bmstr
+    def setstartscan(self,stsc):
+        scstr = str(stsc)
+        #Assume format is YYMMDDXXX 
+        #this is valid from Feb 2018
+        #Jan and earlier had only two scan numbers
+        #I think just focusing on that date range (at least to start) is okay
+        if len(scstr) != 9:
+            print "Start scan must be of format 'YYMMDDXXX'"
+        elif scstr[0:2] != '18': #can add more years later if need be
+            print "Start scan must be of format 'YYMMDDXXX', with a year of 18"
+        else:
+            self.startscan = scstr
+    def setendscan(self,edsc):
+        scstr = str(edsc)
+        #Assume format is YYMMDDXXX 
+        #this is valid from Feb 2018
+        #Jan and earlier had only two scan numbers
+        #I think just focusing on that date range (at least to start) is okay
+        if len(scstr) != 9:
+            print "Start scan must be of format 'YYMMDDXXX'"
+        elif scstr[0:2] != '18': #can add more years later if need be
+            print "Start scan must be of format 'YYMMDDXXX', with a year of 18"
+        else:
+            self.endscan = scstr    
+    def setnscan(self,ns):
+        nsstr = str(ns)
+        #want to check if it is a number
+        #but doesn't work how I expect
+        try:
+            float(nsstr)
+            if float(nsstr) == int(float(nsstr)):
+                self.nscan = nsstr
+            else:
+                new_ns = str(int(float(nsstr)))
+                self.nscan = new_ns
+        except ValueError:
+            print 'nscan must be a number'
+
+            
+"""
+
+scandict = {'startdate':"", 'enddate':"",'beam':"",
+            'startscan':"",'endscan':"",'nscan':""}
+
+
+        
+def add_end_date(scandict,ed):
+    #add enddate
+    if type(sd) is str:
+        #good, check length
+        if len(sd) != 8:
+            print "Enddate must be a string of format 'YYYYMMDD'"
+        elif sd[0:2] != '201':
+            print "Enddate must be a string of format 'YYYYMMDD'"
+        else:
+            scandict["enddate"] = ed
+    else:
+        print "Enddate must be a string of format 'YYYYMMDD'"
+"""
+
+"""    
+def add_beam():
+    #add beam
+    
+def add_start_scan():
+    #adds tart scan
+    
+def add_end_scan():
+    #add last scan
+    
+def add_n_scan():
+    #add number of scans
+
+
+def get_scan_list():
+    #pass a dictionary with keywords that define what mode I will operate in
+    #all keywords in dictionary, but not all filled in
+    #I want to have two modes of operation:
+    #(1) Give a start scan and end (or total number) to get a series of beams
+    #(2) Give a date range and beam to give all scans 
+    #this function will generate a list of scans I care about
+    #It will be a full directory path list, and will include the
+    #appropriate beam where the source is
+    #Or, I may need the beam list separately - works better w/ Apercal
+    #Would like to exclude long scans - read MS
+    
+    return scan_list,beam_list,time_list
+    
+def copy_scans():
+    #This will use the output of get_scan_list 
+    #to copy the data to where I want it to be
+    #do manually. start w/ happili, expand to ALTA later
+
+def flag_scans():
+    #This will use output of get_scan_list
+    #plus apercal.preflag() to flag the data
+    #Note that this likely will not work until the preflag error is fixed
+    #So I probably won't use it at first    
+
+def convert_scans():
+    #Convert to miriad format
+    
+def calibrate_scans():
+    #This iwll use scan_list,beam_list from get_scan_list
+    #plus apercal.ccal to calibrate the data
+    #HAVE TO CHANGE SOURCE NAME!
+    
+
+
+def get_time_sequence_for_beam():
+    #Get a series of scans for single beam
+    
+def compare_scan_solution_amp():
+    #This will compare scan solutions to reference (given) for amplitude
+    #will return an array that is amp solution for each scan divided by reference
+    #these scans can be separated by time or beam
+    
+def compare_scan_solution_phase():
+    #This will compare scan solutions to reference (given) for phase
+    #will return an array that is phase solution for each scan divided by reference
+    
+def compare_scan_solution_bp_phase():
+    #This will compare scan BP solutions to reference (given) for phase
+    #will return a 2-D array (freq, scan) 
+    #that is BP phase solution for each scan divided by reference 
+        
+def compare_scan_solution_bp_amp():
+    #This will compare scan BP solutions to reference (given) for amp
+    #will return a 2-D array (freq, scan)
+    #that is BP amp solution for each scan divided by reference
+    
+def plot_compare_beam_amp():
+    #this will generate plots that show comparison in amplitude solution
+    #between different scans, labelled/plotted as function of beam
+    
+def plot_compare_beam_phase():
+    #this will generate plots that show comparison in phase solution
+    #between different scans, labelled/plotted as function of beam
+    
+def plot_compare_beam_bp_amp():
+    #this will generate plots that show comparison in BP amplitude solution
+    #between different scans, labelled/plotted as function of beam
+    
+def plot_compare_beam_bp_phase():
+    #this will generate plots that show comparison in BP phase solution
+    #between different scans, labelled/plotted as function of beam
+    
+def plot_compare_time_amp():
+    #this will generate plots that show comparison in amplitude solution
+    #between different scans, labelled/plotted as function of time
+    
+def plot_compare_time_phase():
+    #this will generate plots that show comparison in phase solution
+    #between different scans, labelled/plotted as function of time
+    
+def plot_compare_time_bp_amp():
+    #this will generate plots that show comparison in BP amplitude solution
+    #between different scans, labelled/plotted as function of time
+    
+def plot_compare_time_bp_phase():
+    #this will generate plots that show comparison in BP phase solution
+    #between different scans, labelled/plotted as function of time
+    
+"""
+        
--- CROSSCAL/test_crosscal.py
+++ CROSSCAL/test_crosscal.py
@@ -1,1 +1,203 @@
 #collection of tests for crosscal.py
+
+#I will create a separate TestObject for each function in crosscal.py
+#That way, I can just run tests on a single object if I only update one function
+
+import sys
+sys.path.append('/home/adams/apertif/CROSSCAL')
+import crosscal as cc
+
+class TestClass_ScanSpecification(object):
+    #define scanspecification as a class
+    #with ability to set different attributes
+    #all initialized to empty strings
+    def test_initialize_startdate_ScanSpec(self):
+        scans = cc.ScanSpecification()
+        assert scans.startdate == ''
+    def test_initialize_enddate_ScanSpec(self):
+        scans = cc.ScanSpecification()
+        assert scans.enddate == ''
+    def test_initialize_beam_ScanSpec(self):
+        scans = cc.ScanSpecification()
+        assert scans.beam == ''        
+    def test_initialize_startscan_ScanSpec(self):
+        scans = cc.ScanSpecification()
+        assert scans.startscan == ''
+    def test_initialize_endscan_ScanSpec(self):
+        scans = cc.ScanSpecification()
+        assert scans.endscan == ''        
+    def test_initialize_nscan_ScanSpec(self):
+        scans = cc.ScanSpecification()
+        assert scans.nscan == ''  
+    def test_set_bad_string_startdate(self):
+        scans=cc.ScanSpecification()
+        scans.setstartdate('nd39sl2k')
+        assert scans.startdate == ''
+    def test_set_short_startdate(self):
+        scans =cc.ScanSpecification()
+        scans.setstartdate('180512')
+        assert scans.startdate == ''
+    def test_set_early_startdate(self):
+        scans=cc.ScanSpecification()
+        scans.setstartdate('20050812')
+        assert scans.startdate == ''
+    def test_set_good_startdate(self):
+        scans=cc.ScanSpecification()
+        scans.setstartdate('20180512')
+        assert  scans.startdate ==  '20180512'
+    def test_set_long_string_startdate(self):
+        scans =cc.ScanSpecification()
+        scans.setstartdate('2018asdkglbnasldkgjaesltkj')
+        assert scans.startdate == ''
+    def test_set_number_startdate(self):
+        scans =cc.ScanSpecification()
+        scans.setstartdate(20180512)
+        assert scans.startdate == '20180512'
+    def test_set_bad_string_enddate(self):
+        scans=cc.ScanSpecification()
+        scans.setenddate('nd39sl2k')
+        assert scans.enddate == ''
+    def test_set_short_enddate(self):
+        scans =cc.ScanSpecification()
+        scans.setenddate('180512')
+        assert scans.enddate == ''
+    def test_set_early_enddate(self):
+        scans=cc.ScanSpecification()
+        scans.setenddate('20050812')
+        assert scans.enddate == ''
+    def test_set_good_enddate(self):
+        scans=cc.ScanSpecification()
+        scans.setenddate('20180512')
+        assert  scans.enddate ==  '20180512'
+    def test_set_long_string_enddate(self):
+        scans =cc.ScanSpecification()
+        scans.setenddate('2018asdkglbnasldkgjaesltkj')
+        assert scans.enddate == ''        
+    def test_set_number_wenddate(self):
+        scans =cc.ScanSpecification()
+        scans.setenddate(20180512)
+        assert scans.enddate == '20180512'
+    def test_set_beam_good_string(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam('03')
+        assert scans.beam == '03'
+    def test_set_beam_single_int_string(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam('3')
+        assert scans.beam == '03'
+    def test_set_beam_single_integer(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam(3)
+        assert scans.beam == '03'
+    def test_set_beam_double_integer(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam(15)
+        assert scans.beam == '15'
+    def test_set_beam_too_large_string(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam('42')
+        assert scans.beam == ''
+    def test_set_beam_too_large_int(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam(42)
+        assert scans.beam == ''
+    def test_set_beam_too_small_int(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam(-1)
+        assert scans.beam == ''
+    def test_set_beam_too_small_string(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam(-1)
+        assert scans.beam == ''
+    def test_set_beam_float_string(self):
+        scans = cc.ScanSpecification()
+        scans.setbeam('21.5')
+        assert scans.beam == ''
+    def test_set_short_startscan(self):
+        scans =cc.ScanSpecification()
+        scans.setstartscan('18051201')
+        assert scans.startscan == ''
+    def test_set_early_startscan(self):
+        scans=cc.ScanSpecification()
+        scans.setstartscan('080512001')
+        assert scans.startscan == ''
+    def test_set_good_startscan(self):
+        scans=cc.ScanSpecification()
+        scans.setstartscan('180512001')
+        assert  scans.startscan ==  '180512001'
+    def test_set_long_string_startscan(self):
+        scans =cc.ScanSpecification()
+        scans.setstartscan('18asdkglbnasldkgjaesltkj')
+        assert scans.startscan == ''
+    def test_set_number_startscan(self):
+        scans =cc.ScanSpecification()
+        scans.setstartscan(180512001)
+        assert scans.startscan == '180512001'        
+    def test_set_short_endscan(self):
+        scans =cc.ScanSpecification()
+        scans.setendscan('18051201')
+        assert scans.endscan == ''
+    def test_set_early_endscan(self):
+        scans=cc.ScanSpecification()
+        scans.setendscan('080512001')
+        assert scans.endscan == ''
+    def test_set_good_endscan(self):
+        scans=cc.ScanSpecification()
+        scans.setendscan('180512001')
+        assert  scans.endscan ==  '180512001'
+    def test_set_long_string_endscan(self):
+        scans =cc.ScanSpecification()
+        scans.setendscan('18asdkglbnasldkgjaesltkj')
+        assert scans.endscan == ''
+    def test_set_number_endscan(self):
+        scans =cc.ScanSpecification()
+        scans.setendscan(180512001)
+        assert scans.endscan == '180512001'         
+    def test_set_nscan_good_int(self):
+        scans = cc.ScanSpecification()
+        scans.setnscan(235)
+        assert scans.nscan == '235'
+    def test_set_nscan_good_string(self):
+        scans = cc.ScanSpecification()
+        scans.setnscan('235')
+        assert scans.nscan == '235'
+    def test_set_nscan_float(self):
+        scans=cc.ScanSpecification()
+        scans.setnscan(235.7)
+        assert scans.nscan == '235'
+    def test_set_nscan_float_string(self):
+        scans = cc.ScanSpecification()
+        scans.setnscan('235.7')
+        assert scans.nscan == '235'
+    def test_set_nscan_bad_string(self):
+        scans = cc.ScanSpecification()
+        scans.setnscan('asgase')
+        assert scans.nscan == ''
+        
+"""        
+    def test_dictionary_add_good_start_date(self):
+        scandict=cc.add_start_date(cc.scandict,'20180512')
+        assert scandict["startdate"] == '20180512'
+        
+    def test_dictionary_add_YY_start_date(self):
+        cc.add_start_date(cc.scandict,'180512')
+        assert cc.scandict["startdate"] == ""
+        
+    def test_dictionary_add_number_start_date(self):
+        cc.add_start_date(cc.scandict,20180512)
+        assert cc.scandict["startdate"] == "20180512"
+      
+    def test_dictionary_add_random_start_date(self):
+        cc.add_start_date(cc.scandict,'a3ks1k0g')
+        assert cc.scandict["startdate"] == ""
+       
+    def test_dictionary_add_too_early_start_date(self):
+        cc.add_start_date(cc.scandict,'20050102')
+        assert cc.scandict["startdate"] == ""
+        
+"""        
+        
+"""
+class TestClass_get_scan_list(object):
+    def test_get_list_by_date_range(self):
+"""
